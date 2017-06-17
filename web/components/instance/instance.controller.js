470ec67fbb0ae86ef2ed8366b8e5fdbadf081a02
Jorge Capoduri <jorge.capoduri@avature.net>
2017-06-17 12:29:05 -0300

    add more instance widgets

--- web/components/instance/instance.controller.js
+++ web/components/instance/instance.controller.js
@@ -1,6 +1,9 @@
 import {Radio, Marionette, _} from '../../vendor/vendor';
 import {BaseController} from '../../modules/controller.base';
 import {InstanceLanding} from './instance.landing';
+import {InstanceEditView} from './instance.edit';
+import {InstanceModel} from './instance.model';
+import FormView from '../shared/form/form.view';
 
 var InstanceController = BaseController.extend({
   baseEndpoint: 'dashboard',
@@ -10,15 +13,15 @@ var InstanceController = BaseController.extend({
     channel.trigger('setView', view);
   },
   showOne: function(id) {
-    var User = new UserModel();
+    var Instance = new InstanceModel();
     if (!!id && parseInt(id)) {
-        User.set('id', id);
-        User.fetch();
+        Instance.set('id', id);
+        Instance.fetch();
     }
     var form = new FormView({
-        title      : User.get('id') ? "Modificar Usuario" : "Agregar Usuario",
-        model      : User,
-        formClass  : UserView,
+        title      : Instance.get('id') ? "Modificar Instancia" : "Agregar Instancia",
+        model      : Instance,
+        formClass  : InstanceEditView,
         doneAction : _.bind(this.goBase, this)
     });
     var channel = this.getChannel();
---
+++ web/components/instance/instance.edit.js
@@ -0,0 +1,15 @@
+import {Marionette, Backbone, Validation, _} from '../../vendor/vendor';
+import Binding from '../helpers/binding';
+import template from './instance.edit.tpl.html';
+import Decorator from '../helpers/error.decorator';
+
+class InstanceEditView extends Marionette.View {
+    constructor() {
+        super({
+            template: template
+        })
+    }
+}
+
+export {InstanceEditView};
+
